2d1b6e9c3ecfa182beadfc7d49bb780f1af0b0eb
Reymark Arsenio <52293784+rarsenio@users.noreply.github.com>
2021-02-23 18:44:24 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBnNvhq0PQCHuDOhm_f6mvoN9JomsDF5ig",
+  authDomain: "innovtestproject.firebaseapp.com",
+  projectId: "innovtestproject",
+  storageBucket: "innovtestproject.appspot.com",
+  messagingSenderId: "420944891456",
+  appId: "1:420944891456:web:017eadbe839df1b0fe59a5"
+};
+firebase.initializeApp(firebaseConfig);
